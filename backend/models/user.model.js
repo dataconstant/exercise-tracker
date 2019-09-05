7b3fd096c4f20be894c9d3caa1f455c849b1ff58
dataconstant <abhishekchetri@outlook.com>
2019-09-05 22:18:52 +1000

    backend completed

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -13,3 +13,7 @@ const userSchema = new Schema({
 }, {timestamps:true}
 
 )
+
+
+
+module.exports =  mongoose.model('User', userSchema);;
--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,17 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/myapp', {useNewUrlParser: true, useCreateIndex:true});
+mongoose.connect('mongodb://localhost:27017/exercise-tracker', {useNewUrlParser: true, useCreateIndex:true});
 const connection = mongoose.connection;
 connection.once('open', ()=>{
     console.log('MongoDB connected')
 })
+
+const exercisesRouter = require('./routes/exercises')
+const usersRouter = require('./routes/users')
+
+app.use('/users', usersRouter)
+app.use('/exercises', exercisesRouter)
 
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`)
